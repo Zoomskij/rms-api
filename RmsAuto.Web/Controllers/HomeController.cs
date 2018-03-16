aed3e8685f20639aa09b693e9e25fdb13b63a221
APotapov <potapov.ao@rmsauto.ru>
2018-03-16 10:20:51 +0300

    added methods

--- RmsAuto.Web/Controllers/HomeController.cs
+++ RmsAuto.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RMSAutoAPI.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,6 +8,7 @@ using System.Web.Mvc;
 
 namespace RMSAutoAPI.Controllers
 {
+
     public class HomeController : Controller
     {
         public ActionResult Index()
@@ -15,10 +17,30 @@ namespace RMSAutoAPI.Controllers
             return View("~/Views/Home/Index.cshtml",null, url);
         }
 
+        
+
+
+        public ActionResult Index2()
+        {
+            var methods = new List<ApiMethod>();
+            methods.Add(new ApiMethod() { Type = "GET", Name = "Ge tBrands", Uri = "/api/articles/{article}/brands", Group = "Articles" });
+            methods.Add(new ApiMethod() { Type = "GET", Name = "Get Spare Parts", Uri = "/api/articles/{article}/brand/{brand}", Group = "Articles" });
+            methods.Add(new ApiMethod() { Type = "GET", Name = "Get Partners", Uri = "/api/Partners", Group = "Partners" });
+
+            return View(methods);
+        }
+
         [ChildActionOnly]
         public ActionResult GetHtmlPage(string path)
         {
             return new FilePathResult(path, "text/html");
+        }
+
+
+        [HttpGet]
+        public PartialViewResult Authorization()
+        {
+            return PartialView();
         }
     }
 }
---
+++ RmsAuto.Web/Models/ApiMethod.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RMSAutoAPI.Models
+{
+    public class ApiMethod
+    {
+        public string Group { get; set; }
+        public string Type { get; set; }
+        public string Name { get; set; }
+        public string Uri { get; set; }
+    }
+}
